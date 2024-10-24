58e1b34a504e21fe55ecbb0c122b3dc82ac85df8
Alex Soleta <alexandrasoleta@gmail.com>
2024-10-24 13:07:05 -0400

    Add toolbar

--- app/src/main/java/com/example/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/example/bitsandpizzas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bitsandpizzas;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
 
     }
 }
