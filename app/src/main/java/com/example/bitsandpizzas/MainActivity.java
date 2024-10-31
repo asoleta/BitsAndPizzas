ca570f29b7f4d808aa4ab8f53e3d1ea6e5ae97a7
Alex Soleta <alexandrasoleta@gmail.com>
2024-10-31 12:52:05 -0400

    Create onCreateOptionsMenu function

--- app/src/main/java/com/example/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/example/bitsandpizzas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bitsandpizzas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,5 +33,11 @@ public class MainActivity extends AppCompatActivity {
         else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
