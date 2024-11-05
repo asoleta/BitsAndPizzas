ed048d1b6960c419c79d34dadeead77021c5730d
Alex Soleta <alexandrasoleta@gmail.com>
2024-11-05 12:53:52 -0500

    Create pasta and store fragment

---
+++ app/src/main/java/com/example/bitsandpizzas/PastaFragment.java
@@ -0,0 +1,30 @@
+package com.example.bitsandpizzas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PastaFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PastaFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.pasta));
+        setListAdapter(adapter);
+        return inflater.inflate(R.layout.fragment_pasta, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/bitsandpizzas/StoreFragment.java
@@ -0,0 +1,30 @@
+package com.example.bitsandpizzas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StoreFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StoreFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.stores));
+        setListAdapter(adapter);
+        return inflater.inflate(R.layout.fragment_store, container, false);
+    }
+}
